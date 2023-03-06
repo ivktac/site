77dfe024f6f31364d40573b56d70b9642babb446
seshotake <seshotake@gmail.com>
2023-03-06 03:24:12 +0200

    remove unnecessary `$nav`

--- layout/header.php
+++ layout/header.php
@@ -1,11 +1,3 @@
-<?php
-$nav = [
-  'main' => 'Main',
-  'about' => 'About',
-  'registration' => 'Registration',
-];
-?>
-
 <header class="header">
   <?php require_once 'layout/sidemenu.php'; ?>
 
